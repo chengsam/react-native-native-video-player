934ceade26dd79aee0da669b79c7661b898b4124
Sam Cheng <samchengmc@redso.com.hk>
2019-03-06 12:59:27 +0800

    Remove unused imports

--- index.ios.js
+++ index.ios.js
@@ -1,13 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
 import { NativeModules } from 'react-native'
 
 const { RNVideoPlayer } = NativeModules
 
-module.exports = {
-  ...RNVideoPlayer,
-
-  showVideoPlayer(url) {
-    return RNVideoPlayer.showVideoPlayer(url)
-  },
-}
+module.exports = RNVideoPlayer;
